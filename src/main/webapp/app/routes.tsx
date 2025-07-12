f808468a76a2d57a0d99e9fedd15553512a9ecf7
DiogoLeonardoo <diogoleo57@gmail.com>
2025-07-12 16:24:09 -0300

    feat: correcao das rotas de login

--- src/main/webapp/app/routes.tsx
+++ src/main/webapp/app/routes.tsx
@@ -31,8 +31,8 @@ const AppRoutes = () => {
   return (
     <div className="view-routes">
       <ErrorBoundaryRoutes>
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
+        <Route index element={<Login />} />
+        <Route path="home" element={<Home />} />
         <Route path="logout" element={<Logout />} />
         <Route path="account">
           <Route
