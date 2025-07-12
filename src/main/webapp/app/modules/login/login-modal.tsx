3ad9235ce26d9479b6c004cd68d23143cccbf8e0
DiogoLeonardoo <diogoleo57@gmail.com>
2025-07-12 16:24:34 -0300

    feat: login page

--- src/main/webapp/app/modules/login/login-modal.tsx
+++ src/main/webapp/app/modules/login/login-modal.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Translate, ValidatedField, translate } from 'react-jhipster';
-import { Alert, Button, Col, Form, Modal, ModalBody, ModalFooter, ModalHeader, Row } from 'reactstrap';
+import { Alert, Button, Col, Form, Row, Container } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { type FieldError, useForm } from 'react-hook-form';
 
 export interface ILoginModalProps {
-  showModal: boolean;
   loginError: boolean;
   handleLogin: (username: string, password: string, rememberMe: boolean) => void;
   handleClose: () => void;
 }
 
-const LoginModal = (props: ILoginModalProps) => {
+const LoginPage = (props: ILoginModalProps) => {
   const login = ({ username, password, rememberMe }) => {
     props.handleLogin(username, password, rememberMe);
   };
@@ -22,90 +21,58 @@ const LoginModal = (props: ILoginModalProps) => {
     formState: { errors, touchedFields },
   } = useForm({ mode: 'onTouched' });
 
-  const { loginError, handleClose } = props;
+  const { loginError } = props;
 
   const handleLoginSubmit = e => {
     handleSubmit(login)(e);
   };
 
   return (
-    <Modal isOpen={props.showModal} toggle={handleClose} backdrop="static" id="login-page" autoFocus={false}>
-      <Form onSubmit={handleLoginSubmit}>
-        <ModalHeader id="login-title" data-cy="loginTitle" toggle={handleClose}>
-          <Translate contentKey="login.title">Sign in</Translate>
-        </ModalHeader>
-        <ModalBody>
-          <Row>
-            <Col md="12">
-              {loginError ? (
-                <Alert color="danger" data-cy="loginError">
-                  <Translate contentKey="login.messages.error.authentication">
-                    <strong>Failed to sign in!</strong> Please check your credentials and try again.
-                  </Translate>
-                </Alert>
-              ) : null}
-            </Col>
-            <Col md="12">
-              <ValidatedField
-                name="username"
-                label={translate('global.form.username.label')}
-                placeholder={translate('global.form.username.placeholder')}
-                required
-                autoFocus
-                data-cy="username"
-                validate={{ required: 'Username cannot be empty!' }}
-                register={register}
-                error={errors.username as FieldError}
-                isTouched={touchedFields.username}
-              />
-              <ValidatedField
-                name="password"
-                type="password"
-                label={translate('login.form.password')}
-                placeholder={translate('login.form.password.placeholder')}
-                required
-                data-cy="password"
-                validate={{ required: 'Password cannot be empty!' }}
-                register={register}
-                error={errors.password as FieldError}
-                isTouched={touchedFields.password}
-              />
-              <ValidatedField
-                name="rememberMe"
-                type="checkbox"
-                check
-                label={translate('login.form.rememberme')}
-                value={true}
-                register={register}
-              />
-            </Col>
-          </Row>
-          <div className="mt-1">&nbsp;</div>
-          <Alert color="warning">
-            <Link to="/account/reset/request" data-cy="forgetYourPasswordSelector">
-              <Translate contentKey="login.password.forgot">Did you forget your password?</Translate>
-            </Link>
-          </Alert>
-          <Alert color="warning">
-            <span>
-              <Translate contentKey="global.messages.info.register.noaccount">You don&apos;t have an account yet?</Translate>
-            </span>{' '}
-            <Link to="/account/register">
-              <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-            </Link>
-          </Alert>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={handleClose} tabIndex={1}>
-            <Translate contentKey="entity.action.cancel">Cancel</Translate>
-          </Button>{' '}
-          <Button color="primary" type="submit" data-cy="submit">
-            <Translate contentKey="login.form.button">Sign in</Translate>
-          </Button>
-        </ModalFooter>
-      </Form>
-    </Modal>
+    <Container className="login-page" fluid>
+      <Row className="justify-content-center">
+        <Col md="6">
+          <Form onSubmit={handleLoginSubmit}>
+            {loginError ? (
+              <Alert color="danger" data-cy="loginError">
+                <Translate contentKey="login.messages.error.authentication">
+                  <strong>Failed to sign in!</strong> Please check your credentials and try again.
+                </Translate>
+              </Alert>
+            ) : null}
+            <ValidatedField
+              name="username"
+              label={translate('global.form.username.label')}
+              placeholder={translate('global.form.username.placeholder')}
+              required
+              autoFocus
+              data-cy="username"
+              validate={{ required: 'Username cannot be empty!' }}
+              register={register}
+              error={errors.username as FieldError}
+              isTouched={touchedFields.username}
+            />
+            <ValidatedField
+              name="password"
+              type="password"
+              label={translate('login.form.password')}
+              placeholder={translate('login.form.password.placeholder')}
+              required
+              data-cy="password"
+              validate={{ required: 'Password cannot be empty!' }}
+              register={register}
+              error={errors.password as FieldError}
+              isTouched={touchedFields.password}
+            />
+            <div className="mt-3">
+              <Button color="primary" type="submit" data-cy="submit" block>
+                <Translate contentKey="login.form.button">Sign in</Translate>
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
-export default LoginModal;
+export default LoginPage;
