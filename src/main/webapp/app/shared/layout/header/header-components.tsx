a624699a222845825cc804ae49a473c20e0723dc
Diogo Leonardo <diego.leonardo@saude.se.gov.br>
2025-07-23 10:21:25 -0300

    feat: estruturação da header do sistema

--- src/main/webapp/app/shared/layout/header/header-components.tsx
+++ src/main/webapp/app/shared/layout/header/header-components.tsx
@@ -7,17 +7,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const BrandIcon = props => (
   <div {...props} className="brand-icon">
-    <img src="content/images/logo-jhipster.png" alt="Logo" />
+    <img src="content/images/logo-header-png.png" style={{ width: '2em', marginBottom: '18px' }} />
   </div>
 );
 
 export const Brand = () => (
   <NavbarBrand tag={Link} to="/" className="brand-logo">
     <BrandIcon />
-    <span className="brand-title">
-      <Translate contentKey="global.title">LeiteVida</Translate>
-    </span>
-    <span className="navbar-version">{VERSION.toLowerCase().startsWith('v') ? VERSION : `v${VERSION}`}</span>
   </NavbarBrand>
 );
 
@@ -28,6 +24,69 @@ export const Home = () => (
       <span>
         <Translate contentKey="global.menu.home">Home</Translate>
       </span>
+    </NavLink>
+  </NavItem>
+);
+
+export const Doadora = () => (
+  <NavItem>
+    <NavLink tag={Link} to="/doadora" className="d-flex align-items-center">
+      <img src="content/images/pregnant.svg" style={{ width: '28px', marginRight: '10px' }} />
+      <span>Doadoras</span>
+    </NavLink>
+  </NavItem>
+);
+
+export const Pacientes = () => (
+  <NavItem>
+    <NavLink tag={Link} to="/paciente" className="d-flex align-items-center">
+      <img src="content/images/pessoas.svg" style={{ width: '23px', marginRight: '10px' }} />
+      <span>Pacientes</span>
+    </NavLink>
+  </NavItem>
+);
+
+export const Coleta = () => (
+  <NavItem>
+    <NavLink tag={Link} to="/coleta" className="d-flex align-items-center">
+      <img src="content/images/mamadeira.svg" style={{ width: '15px', marginRight: '10px' }} />
+      <span>Coletas</span>
+    </NavLink>
+  </NavItem>
+);
+
+export const Distribuicao = () => (
+  <NavItem>
+    <NavLink tag={Link} to="/distribuicao" className="d-flex align-items-center">
+      <img src="content/images/criancanobraco.svg" style={{ width: '15px', marginRight: '10px' }} />
+      <span>Distribuição</span>
+    </NavLink>
+  </NavItem>
+);
+
+export const Estoque = () => (
+  <NavItem>
+    <NavLink tag={Link} to="/estoque" className="d-flex align-items-center">
+      <img src="content/images/frasco.svg" style={{ width: '15px', marginRight: '10px' }} />
+      <span>Estoque</span>
+    </NavLink>
+  </NavItem>
+);
+
+export const Users = () => (
+  <NavItem>
+    <NavLink tag={Link} to="/admin/user-management" className="d-flex align-items-center">
+      <img src="content/images/pessoas.svg" style={{ width: '25px', marginRight: '10px' }} />
+      <span>Usuários</span>
+    </NavLink>
+  </NavItem>
+);
+
+export const Processamentos = () => (
+  <NavItem>
+    <NavLink tag={Link} to="/processamento" className="d-flex align-items-center">
+      <img src="content/images/frascolab.svg" style={{ width: '20px', marginRight: '10px' }} />
+      <span>Processamentos</span>
     </NavLink>
   </NavItem>
 );
